69f4c32448c7f27cd5cd64b9b4870945df90a38c
Mine <mine.kandil@gmail.com>
2020-07-18 17:26:50 +0300

    APIs made db aware

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,13 @@ namespace Persistence
             
         }
         public DbSet<Value> Values { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>().HasData(
+                new Value {Id=1, Name="Value101"},
+                  new Value {Id=2, Name="Value102"},
+                    new Value {Id=3, Name="Value103"}
+            );
+        }
 
     }
 }
