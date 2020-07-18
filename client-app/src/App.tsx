7f80f5a4745850403cca99660b6c8fa5fb7f83f1
Mine <mine.kandil@gmail.com>
2020-07-18 21:01:09 +0300

    creating dummy values list

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { cars } from './demo';
-import { CarItem } from './CarItem';
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map((car) => (
-        <li><CarItem car={car}/></li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+class App extends Component{
+  state={
+    values:[]
+  }
+
+  componentDidMount(){
+    this.setState({
+      values :[{id:1,name:'value101'},{id:2,name:'value102'}]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"></img>
+        <ul>
+          {this.state.values.map((value:any) =>(
+            <li>{value.name}</li>
+          ))}
+        </ul>
+        </header>
+      </div>
+    );
+  }
+} 
 
 export default App;
