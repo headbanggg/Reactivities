9542208882527132c0a3afd3d7cc7d32cff7b53f
Mine <mine.kandil@gmail.com>
2021-01-02 16:54:52 +0300

    add create Activity API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class ActivitiesController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -21,10 +22,18 @@ namespace API.Controllers
         public async Task<ActionResult<List<Activity>>> List(){
             return await _mediator.Send(new List.Query());
         }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
             return await _mediator.Send(new Details.Query{Id=id});
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        
     }
 }
