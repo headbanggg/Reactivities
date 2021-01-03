32549a9d1c36393fdd5a72985d63abba7623e321
Mine <mine.kandil@gmail.com>
2021-01-03 17:42:39 +0300

    add loading indicator for submitting data

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
 
   const[editMode, setEditMode]= useState(false);
   const[loading, setLoading] = useState(true);
+  const[submitting, setSubmitting] = useState(false);
 
   const handleSelectActivity =(id: string) =>{
     setSelectedActivity(activities.filter(a => a.id ===id)[0])
@@ -25,26 +26,29 @@ const App = () => {
   }
 
   const handleCreateActivity =(activity:IActivity) => {
+    setSubmitting(true);
     agent.Activities.create(activity).then (()=>{
       setActivities ([...activities ,activity])
       setSelectedActivity(activity);
       setEditMode(false);
-    })
+    }).then(()=>setSubmitting(false))
   }
 
   const handleEditActivity =(activity:IActivity) => {
+    setSubmitting(true);
     agent.Activities.update(activity).then (() =>{
       setActivities ([...activities.filter(a=> a.id!== activity.id), activity])
       setSelectedActivity(activity);
       setEditMode(false);
-    })
+    }).then(()=>setSubmitting(false))
 
   }
 
   const handleDeleteActivity = (id:string) => {
+    setSubmitting(true);
     agent.Activities.delete(id).then(()=>{
       setActivities([...activities.filter (a => a.id !== id)])
-    })
+    }).then(()=>setSubmitting(false))
   }
 
   useEffect(()=>{
@@ -76,6 +80,7 @@ const App = () => {
           createActivity ={handleCreateActivity}
           editActivity ={handleEditActivity}
           deleteActivity= {handleDeleteActivity}
+          submitting ={submitting}
           />
         </Container>
       </Fragment>
